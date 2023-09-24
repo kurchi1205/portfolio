d8543d57411d0316a85443fde9b82efb0dfe8ac6
Sourabh <96406425+sourabhHS@users.noreply.github.com>
2023-09-24 17:14:09 +0530

    shadow

--- src/components/Sections/section/Projects/ProjectCard/index.jsx
+++ src/components/Sections/section/Projects/ProjectCard/index.jsx
@@ -2,10 +2,7 @@ const ProjectCard = ({ project }) => {
     return (
         <div className="col-md-4 mb-5">
             <div className="card" style={{borderRadius:"15px", maxWidth: "21.875rem"}}>
-                {project.projectImage && <div className="card-image-container">
-                    <img src={process.env.PUBLIC_URL + "/images/Projects/" + project.projectImage} className="card-img-top card-img" alt={project.projectImage} />
-                </div>}
-                <div className="card-body">
+                <div className="card-body white-box-without-border">
                     <h2 className="fw-bolder">{project.projectName}</h2>
                     <p className="project-desc text-secondary">{project.projectDescription}</p>
                     <a href={project.projectSource} className="button-link project-source" target="_blank" rel="noreferrer">View Project</a>
