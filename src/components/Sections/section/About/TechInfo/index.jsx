3af59e6c26ee7a1ffb13b4d2bb7dd18ce42d439d
Sourabh <96406425+sourabhHS@users.noreply.github.com>
2023-10-08 19:27:00 +0530

    feat: Tech info section component - rendering skills, tools and domains

---
+++ src/components/Sections/section/About/TechInfo/index.jsx
@@ -0,0 +1,30 @@
+const Divider = () => {
+	return <div class="progress"></div>
+}
+
+const TechInfo = ({ data }) => {
+	console.log('data', data)
+	return (
+		<div className="col-md-6">
+			{data.map(({ name, items, flatList }, index) => {
+				return (
+					<div key={'tech_' + index} style={{ marginBottom: '30px' }}>
+						<h4 style={{ fontWeight: 'bold' }}>{name}</h4>
+						{flatList ? (
+							<p>{items.join(', ')}</p>
+						) : (
+							<ul>
+								{items.map((item, idx) => {
+									return <li key={'tech_item_' + idx} style={{ marginBottom: '8px' }}>{`${item.name}: ${item.items.join(', ')}`}</li>
+								})}
+							</ul>
+						)}
+						<Divider />
+					</div>
+				)
+			})}
+		</div>
+	)
+}
+
+export default TechInfo
--- src/components/Sections/section/About/index.jsx
+++ src/components/Sections/section/About/index.jsx
@@ -1,25 +1,27 @@
 import Info from './Info'
 import MiscInfo from './MiscInfo'
 import Card from '../../../Common/Card'
+import TechInfo from './TechInfo'
 
 import './about.css'
 
 const About = ({ data }) => {
-    const { info, sectionBackground } = data.sectionValue
-    return (
-        <section id="about" className={sectionBackground}>
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-12">
-                        <Card sectionClassName='about-flex'>
-                            <Info data={info} />
-                            <MiscInfo />
-                        </Card>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+	const { info, sectionBackground, tech } = data.sectionValue
+	return (
+		<section id="about" className={sectionBackground}>
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-12">
+						<Card sectionClassName="about-flex">
+							<Info data={info} />
+							<TechInfo data={tech} />
+							{/* <MiscInfo /> */}
+						</Card>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default About
